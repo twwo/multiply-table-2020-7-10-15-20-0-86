f4416325f9d498bc5e7cddc8b0d0c4111c62c679
SHAO LI (ITA-ISDC-ISD-OOCLL/ZHA) <LISH5@OOCL.COM>
2020-07-21 18:21:12 +0800

    [1][implement multiply table][feature]

--- main.js
+++ main.js
@@ -1,5 +1,38 @@
 function createMultiplyTable(start, end) {
+  if (isValid(start, end)) {
+    return generateAllLines(start, end); 
+  }
   return null;
+}
+
+function isValid(start, end) {
+  if (start <= end && start <= 1000 && start >= 0
+    && end <= 1000 && end >= 0) {
+    return true;
+  }
+  return false;
+}
+
+function generateAllLines(start, end) {
+  let str = "";
+  for (let i = start; i <= end; i++) {
+    str = str + generateLine(start, i);
+    if (i != end) {
+      str += "\n";
+    }
+  }
+  return str;
+}
+
+function generateLine(lineStart, lineEnd) {
+  let str = "";
+  for (let i = start; i <= end; i++) {
+    str = str + i + "*" + end + "=" + i * end;
+    if (i != end) {
+      str += "\t";
+    }
+  }
+  return str;
 }
 
 module.exports = {
